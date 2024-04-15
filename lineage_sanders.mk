7790941983f5fabaa761f7cf05f5f5ffb6c88534
Sander Koenders <hello@sanderkoenders.nl>
2024-04-15 16:18:38 +0200

    Inherit common lineage phone

--- lineage_sanders.mk
+++ lineage_sanders.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/motorola/sanders/device.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full.mk))
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
